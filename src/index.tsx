c726c87e8010261972849ee49d1130addcf86bcd
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-07-14 11:18:00 +0530

    fix remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import { Web3Provider } from "@ethersproject/providers";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./state";
 import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
-import { Provider } from "react-redux";
 import { PangolinProvider } from "@pangolindex/components";
 import getLibrary from "./utils/getLibrary";
 
@@ -26,9 +24,7 @@ const AppProvider = () => {
 ReactDOM.render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Provider store={store}>
-        <AppProvider />
-      </Provider>
+      <AppProvider />
     </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById("root")
